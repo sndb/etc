67fdb0437a179b3ac61d5b4d00b469e4a0067b4f
sndb <sndb@sndb.xyz>
2020-11-28 00:58:41 +0300

    move to dwm

--- bin/.local/bin/baraction.sh
+++ bin/.local/bin/baraction.sh
@@ -2,57 +2,41 @@
 
 sleep_sec=3
 
-bg_def="+@bg=0;"
-bg_blk="+@bg=1;"
-bg_red="+@bg=2;"
-bg_grn="+@bg=3;"
-bg_ylw="+@bg=4;"
-bg_blu="+@bg=5;"
-bg_mag="+@bg=6;"
-bg_cyn="+@bg=7;"
-bg_wht="+@bg=8;"
-
-fg_def="+@fg=0;"
-fg_blk="+@fg=1;"
-fg_red="+@fg=2;"
-fg_grn="+@fg=3;"
-fg_ylw="+@fg=4;"
-fg_blu="+@fg=5;"
-fg_mag="+@fg=6;"
-fg_cyn="+@fg=7;"
-fg_wht="+@fg=8;"
-
 get_volume () {
-	local volume mute mute_message
+	local volume mute mute_message ret
 	volume="$(pulsemixer --get-volume)"
 	mute="$(pulsemixer --get-mute)"
-	mute_message="${fg_red} muted${fg_def}"
+	mute_message="muted"
 
 	if ((mute == 1)); then
-		local vol="$mute_message"
+		ret="$mute_message"
 	else
-		local vol="${fg_blu} ${volume% *}${fg_def}"
+		ret="${volume% *}"
 	fi
 
-	echo "$vol"
+	echo "$ret"
 }
 
 get_vpn_status () {
 	if pidof "openvpn"; then
-		echo "${fg_grn} Y (OVPN)${fg_def}"
+		echo "Y (OVPN)"
 	elif ip link | grep -qw wg0; then
-		echo "${fg_grn} Y (WG)${fg_def}"
+		echo "Y (WG)"
 	else
-		echo "${fg_red} N${fg_def}"
+		echo "N"
 	fi
 }
 
 get_netstat () {
 	read -r rx tx <<< "$(traffic.sh)"
-	echo "${fg_blu} ↓ $((rx/1024))${fg_def} KiB${fg_mag} ↑ $((tx/1024))${fg_def} KiB"
+	echo "↓ $((rx/1024)) KiB ↑ $((tx/1024)) KiB"
+}
+
+get_time () {
+	date +"%Y-%m-%d %H:%M"
 }
 
 while :; do
-	echo -e "Net:$(get_netstat) - VPN:$(get_vpn_status) - Vol%:$(get_volume) -"
+	xsetroot -name "Net: $(get_netstat)   VPN: $(get_vpn_status)   Vol%: $(get_volume)   $(get_time)"
 	sleep $sleep_sec
 done
