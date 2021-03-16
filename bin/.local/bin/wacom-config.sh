f4e5dccf013d22f6514b6bc89170dd67a6538597
sndb <sndb@sndb.xyz>
2021-03-16 11:01:54 +0300

    replace tablet settings with separate script

---
+++ bin/.local/bin/wacom-config.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# configure wacom tablet
+set -e
+
+for i in $(seq 10); do
+	if xsetwacom list devices | grep -q "Wacom"; then
+		break
+	fi
+	sleep 1
+done
+
+list="$(xsetwacom list devices)"
+pad="$(echo "$list" | awk '/pad/{print $7}')"
+stylus="$(echo "$list" | awk '/stylus/{print $7}')"
+
+if [ -z "$pad" ]; then
+	exit
+fi
+
+xsetwacom set "$stylus" MapToOutput HDMI-A-1
+xsetwacom set "$stylus" Area 0 0 15200 8550
