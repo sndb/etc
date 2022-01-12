215b14b7938e2b41dbbb26c01c66ea86f79da244
sndb <sndb@sndb.xyz>
2022-01-12 04:51:43 +0300

    use 2 space indent for markdown

--- neovim/.config/nvim/lua/autocommands.lua
+++ neovim/.config/nvim/lua/autocommands.lua
@@ -1,3 +1,2 @@
 vim.cmd("autocmd TermOpen * startinsert")
-vim.cmd("autocmd FileType html,css,scss,javascript setlocal expandtab shiftwidth=2 softtabstop=2")
-vim.cmd("autocmd FileType markdown setlocal expandtab shiftwidth=4 softtabstop=4")
+vim.cmd("autocmd FileType html,css,scss,javascript,markdown setlocal expandtab shiftwidth=2 softtabstop=2")
