1777ff96fa88a5f810bd8fdb26d65bec716d48af
sndb <sndb@sndb.xyz>
2022-02-07 23:45:18 +0300

    alacritty -> st

--- dwm/config.h
+++ dwm/config.h
@@ -60,7 +60,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 static const char *lockcmd[] = { "slock", NULL };
 static const char *shotcmd[] = { "shot.sh", NULL };
