61e9c7aa64648c4206a62dca98d4aa2f7f3cb7c2
sndb <sndb@sndb.xyz>
2022-10-07 20:10:15 +0300

    decrease ui font size

--- dwm/config.h
+++ dwm/config.h
@@ -7,7 +7,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char font[]            = "UW Ttyp0:size=10";
+static const char font[]            = "UW Ttyp0:size=8";
 static const char *fonts[]          = { font };
 static const char col_norm_fg[]     = "#bfbfbf";
 static const char col_norm_bg[]     = "#000000";
