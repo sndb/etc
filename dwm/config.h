57999d3f99998ca7fa194dc5726a9aba41b52561
sndb <sndb@sndb.xyz>
2022-10-25 16:22:32 +0300

    2 px borders

--- dwm/config.h
+++ dwm/config.h
@@ -3,7 +3,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
