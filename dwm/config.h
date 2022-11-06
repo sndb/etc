01047b9ea001691609622841a6825306d1d68c99
sndb <sndb@sndb.xyz>
2022-11-06 11:01:53 +0300

    new scripts: radio, sbhosts; dwm XK_grave as XK_0

--- dwm/config.h
+++ dwm/config.h
@@ -101,6 +101,8 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask, XK_space,                togglefloating, {0} },
 	{ MODKEY,           XK_0,                    view,           {.ui = ~0} },
 	{ MODKEY|ShiftMask, XK_0,                    tag,            {.ui = ~0} },
+	{ MODKEY,           XK_grave,                view,           {.ui = ~0} },
+	{ MODKEY|ShiftMask, XK_grave,                tag,            {.ui = ~0} },
 	{ MODKEY,           XK_comma,                focusmon,       {.i = -1} },
 	{ MODKEY,           XK_period,               focusmon,       {.i = +1} },
 	{ MODKEY|ShiftMask, XK_comma,                tagmon,         {.i = -1} },
