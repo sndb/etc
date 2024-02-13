5973ebdef837cf5c1cbbbded6cfdbeea2b4fee3b
sndb <sndb@sndb.xyz>
2024-02-13 20:14:16 +0300

    increase font size

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=12";
+static char *font = "monospace:pixelsize=14";
 static int borderpx = 2;
 
 /*
