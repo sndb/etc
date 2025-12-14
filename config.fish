8ff54dc6a8d2f9daf8ed78363abd9e3924115be8
sndb <ckyg@proton.me>
2025-12-14 16:36:40 +0300

    add fish config

---
+++ config.fish
@@ -0,0 +1,25 @@
+if status is-interactive
+    alias vim 'nvim'
+    alias mkd 'mkdir -vp'
+    alias cp 'cp -vi'
+    alias mv 'mv -vi'
+    alias rm 'rm -vI'
+    alias diff 'diff --color=auto'
+    alias grep 'grep --color=auto'
+    alias ip 'ip -color=auto'
+    alias ls 'ls --color=auto'
+    alias l 'ls -lhvFA --group-directories-first'
+    alias .. 'cd ..'
+    alias ... 'cd ../..'
+    alias .... 'cd ../../..'
+    alias ..... 'cd ../../../..'
+    alias ...... 'cd ../../../../..'
+
+    function cl
+        cd $argv && l
+    end
+
+    set -gx FZF_CTRL_T_OPTS "--walker file,dir,follow --preview 'bat --plain --color=always {}'"
+    set -gx FZF_ALT_C_OPTS "--walker dir,follow"
+    fzf --fish | source
+end
